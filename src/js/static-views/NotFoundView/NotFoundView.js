dfef28f316320d133a952910a27fedc2c7717f67
chandransh <chandransh.s@gmail.com>
2016-12-07 05:37:36 +0530

    Top spacing added

--- src/js/static-views/NotFoundView/NotFoundView.js
+++ src/js/static-views/NotFoundView/NotFoundView.js
@@ -5,8 +5,10 @@ export default class NotFoundView extends Component {
   render () {
     return (
       <div className='container text-center'>
-        <h1>404 page!</h1>
-        <hr />
+        <br/>
+        <br/>
+        <h1>404</h1>
+        <hr/>
         <Link to='/'>Back To Home page</Link>
       </div>
     );
