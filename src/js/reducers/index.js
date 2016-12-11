56d8cf825d0fbfe4ff8ccaea148b29d0b8d7d2ef
chandransh <chandransh.s@gmail.com>
2016-12-11 19:57:21 +0530

    First build

--- src/js/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-
-import getTracksByGenre from './getTracksByGenre';
-
-const rootReducer = combineReducers({
-  getTracksByGenre
-});
-
-export default rootReducer;
--- src/js/utils/index.js
+++
@@ -1,1 +0,0 @@
-export helpers from './helpers';
